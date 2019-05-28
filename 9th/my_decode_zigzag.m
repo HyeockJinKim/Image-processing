4062ed5aeea921a964112ba4ce8dcb4c09e6a598
HyeockJinKim <kherootz@gmail.com>
2019-05-28 13:02:19 +0900

    Add zigzag scanning
    zigzag scanning for removing zero

---
+++ 9th/my_decode_zigzag.m
@@ -0,0 +1,42 @@
+function block = my_decode_zigzag(zigzag)
+% Decode Zigzag
+% zigzag : 
+
+block = double(zeros(8));
+index = 1;
+for i = 1:8
+    if mod(i, 2) == 0
+        x = 1:i;
+        y = i:-1:1;
+    else
+        x = i:-1:1;
+        y = 1:i;
+    end
+    for j = 1:i
+        block(x(j), y(j)) = zigzag.get(index);
+        index = index + 1;
+        if index > zigzag.leng()
+            return
+        end
+    end
+end
+
+for i = 2:8
+    if mod(i, 2) == 0
+        x = i:8;
+        y = 8:-1:i;
+    else
+        x = 8:-1:i;
+        y = i:8;
+    end
+    for j = 1:9-i
+        block(x(j), y(j)) = zigzag.get(index);
+        index = index + 1;
+        if index > zigzag.leng()
+            return
+        end
+    end
+end
+
+end
+
---
+++ 9th/my_zigzag.m
@@ -0,0 +1,41 @@
+function zigzag = my_zigzag(block)
+% Apply zigzag scanning
+% block  : 8 x 8 block for JPEG
+% zigzag : 
+
+zigzag = MyList();
+for i = 1:8
+    if mod(i, 2) == 0
+        x = 1:i;
+        y = i:-1:1;
+    else
+        x = i:-1:1;
+        y = 1:i;
+    end
+    for j = 1:i
+        zigzag = zigzag.add(block(x(j), y(j)));
+    end
+end
+
+for i = 2:8
+    if mod(i, 2) == 0
+        x = i:8;
+        y = 8:-1:i;
+    else
+        x = 8:-1:i;
+        y = i:8;
+    end
+    for j = 1:9-i
+        zigzag = zigzag.add(block(x(j), y(j)));
+    end
+end
+
+for i = 64:-1:1
+    if zigzag.get(i) ~= 0
+        zigzag = zigzag.cut(i);
+        return;
+    end
+end
+
+end
+
